3cb16911c81087c7e28b545d1c75acbcd829b587
filip-herceg <pvt.filip.herceg@gmail.com>
2023-04-19 16:18:05 +0200

    Implemented a new function get_layer_args to retrieve the number and names of arguments expected by a PyTorch layer. Updated the get_layer function to use get_layer_args and to properly handle required arguments for all supported layer types.

--- utils/GetLayerHelper.py
+++ utils/GetLayerHelper.py
@@ -23,24 +23,18 @@ def get_layer(name, input_shape, layer_units, **kwargs):
     if name in get_available_layer_types():
         layer_class = getattr(nn, name)
 
-        # Check if the layer class requires any additional arguments beyond input_shape and layer_units
-        signature = inspect.signature(layer_class.__init__)
-        if len(signature.parameters) > 2:
-            # The layer class has additional arguments beyond input_shape and layer_units
-            # Get the names and default values of the required arguments
-            required_args = list(signature.parameters.keys())[2:]
-            default_values = list(signature.parameters.values())[2:]
-            default_kwargs = {k: v.default for k, v in zip(required_args, default_values) if v.default is not inspect.Parameter.empty}
+        # Get the expected arguments for the layer class
+        layer_args = get_layer_args(layer_class)
 
-            # Make sure all required arguments are present in kwargs
-            missing_args = set(required_args) - set(kwargs.keys())
-            if missing_args:
-                raise ValueError(f"Layer type '{name}' is missing required arguments: {missing_args}")
+        # Check if all required arguments are present in kwargs
+        missing_args = set(layer_args['required']) - set(kwargs.keys())
+        if missing_args:
+            raise ValueError(f"Layer type '{name}' is missing required arguments: {missing_args}")
 
-            # Use the default values for any missing optional arguments
-            for arg, default_value in default_kwargs.items():
-                if arg not in kwargs:
-                    kwargs[arg] = default_value
+        # Use the default values for any missing optional arguments
+        for arg, default_value in layer_args['optional'].items():
+            if arg not in kwargs:
+                kwargs[arg] = default_value
 
         # Call the layer constructor with the specified arguments
         return layer_class(input_shape, layer_units, **kwargs)
@@ -62,3 +56,25 @@ def get_available_layer_types():
                 layer_types.append(name)
     return layer_types
 
+
+def get_layer_args(layer_class):
+    """
+    Returns a dictionary of the expected arguments for a given layer class.
+
+    Args:
+        layer_class (nn.Module): The PyTorch layer class.
+
+    Returns:
+        dict: A dictionary with two keys: 'required' and 'optional'. The value of 'required' is a list of required
+        argument names for the layer, and the value of 'optional' is a dictionary of optional argument names and
+        their default values.
+    """
+    signature = inspect.signature(layer_class.__init__)
+    required_args = []
+    optional_args = {}
+    for param in signature.parameters.values():
+        if param.default == inspect.Parameter.empty:
+            required_args.append(param.name)
+        else:
+            optional_args[param.name] = param.default
+    return {'required': required_args, 'optional': optional_args}
